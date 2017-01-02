ec72f4cf79d863e80382e7b9ded4084f91fe1f04
Konstantinos <kpaidarakis@gmail.com>
2017-01-02 20:21:17 +0200

    load the 3 fonts through Main class & set diff font on title

--- src/pomodoro/Main.java
+++ src/pomodoro/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -11,8 +12,11 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/main-window.fxml"));
+        Font.loadFont(getClass().getResource("/fonts/Ewert-Regular.ttf").toExternalForm(),10.0);
+        Font.loadFont(getClass().getResource("/fonts/VarelaRound-Regular.ttf").toExternalForm(),10.0);
         primaryStage.setTitle("Pomodoro Application");
         primaryStage.setScene(new Scene(root, 500.0, 500.0));
+        primaryStage.setResizable(false);
         primaryStage.show();
     }
 
