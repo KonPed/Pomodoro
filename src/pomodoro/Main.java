bb505997fe4648287d4d064aa4c6e4c6be3d0ece
Konstantinos <kpaidarakis@gmail.com>
2017-01-02 15:55:52 +0200

    set width & height to 500 & added a Vbox as the first container

--- src/pomodoro/Main.java
+++ src/pomodoro/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/main-window.fxml"));
         primaryStage.setTitle("Pomodoro Application");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 500.0, 500.0));
         primaryStage.show();
     }
 
