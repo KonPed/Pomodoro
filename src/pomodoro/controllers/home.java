8bc3464fb2232f0465626fd0f2faa37ad181738b
Konstantinos <kpaidarakis@gmail.com>
2017-01-08 13:33:05 +0200

    create the prepareAttempt method in home Controller.

--- src/pomodoro/controllers/home.java
+++ src/pomodoro/controllers/home.java
@@ -1,7 +1,15 @@
-package pomodoro.controller;
+package pomodoro.controllers;
+
+import pomodoro.model.Attempt;
+import pomodoro.model.AttemptKind;
 
 /**
  * Created by konstantinos on 1/8/2017.
  */
 public class home {
+    private Attempt currentAttempt;
+
+    private void prepareAttempt(AttemptKind attemptKind) {
+    currentAttempt = new Attempt(attemptKind, "");
+    }
 }
